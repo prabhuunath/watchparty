6b43bda02b1e7cb079e06f24488d2513f6d36a68
prabhuunath <dharamnathprasad2@gmail.com>
2024-05-23 00:58:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    socket.on('play', (data) => {
+        io.emit('play', data);
+    });
+
+    socket.on('pause', () => {
+        io.emit('pause');
+    });
+
+    socket.on('seek', (time) => {
+        io.emit('seek', time);
+    });
+
+    socket.on('changeVideo', (videoId) => {
+        io.emit('changeVideo', videoId);
+    });
+
+    socket.on('offer', (offer) => {
+        socket.broadcast.emit('offer', offer);
+    });
+
+    socket.on('answer', (answer) => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('candidate', (candidate) => {
+        socket.broadcast.emit('candidate', candidate);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
